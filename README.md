cbf4f0e73c0ed9453c918edde24d4a183ad374c6
jonathanw82 <jonwhewaycode@outlook.com>
2021-01-26 11:27:29 +0000

    sent to heroku

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # eastbristolhops-sales
 
 E-commerse site for selling hops
+
+
+manage.py oscar_populate_countries
--- eastbristolhops_sales/settings.py
+++ eastbristolhops_sales/settings.py
@@ -26,8 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('APM_SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = 'DEVELOPMENT' in os.environ
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 ALLOWED_HOSTS = ['eastbristolhops-sales.herokuapp.com','127.0.0.1']
 
 
